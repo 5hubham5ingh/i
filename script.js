7fa040a3a910ca78c1d1b09223c7eea8dd0a85c4
Shubham Singh <128460722+5hubham5ingh@users.noreply.github.com>
2024-05-28 17:29:06 +0530

    snackbars and summary page is done

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Select the body element
-//     var sectionElements = document.getElementsByTagName("section");
-//     // Convert HTMLCollection to an array
-//     var sectionArray = Array.from(sectionElements);
-//     // Add an event listener to each section element to trigger fullscreen
-//     sectionArray.forEach(element => element.addEventListener("click", function() {
-//         var element = document.documentElement;
-//         if (element.requestFullscreen) {
-//           element.requestFullscreen();
-//         } else if (element.mozRequestFullScreen) { /* Firefox */
-//           element.mozRequestFullScreen();
-//         } else if (element.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
-//           element.webkitRequestFullscreen();
-//         } else if (element.msRequestFullscreen) { /* IE/Edge */
-//           element.msRequestFullscreen();
-//         }
-//     }));
-// });
-
 
 const wait = async (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
+const snackbar = document.getElementById("snackbar");
+const snackbar2 = document.getElementById("fullscreen");
 const showSnackbar = async () => {
-  const snackbar = document.getElementById("snackbar");
-  //  await wait(7000)
+  await wait(7000)
   snackbar.className = "show";
-  await wait(7000);
-  return snackbar.className = snackbar.className.replace("show", "");
 };
 
-window.onload = () => {
-  showSnackbar();
+
+const askForFullScreen = async () => {
+  snackbar.className = snackbar.className.replace("show", "");
+  snackbar2.className = "show";
+  await wait(7000);
+  snackbar2.className = snackbar2.className.replace("show", "");
 };
+
+const showFullScreen = () => {
+  snackbar2.className = snackbar2.className.replace("show", "");
+  let element = document.documentElement;
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+}
+showSnackbar();

3d0288c2624475c8b9dc886f80da8457afb80903
Shubham Singh <128460722+5hubham5ingh@users.noreply.github.com>
2024-05-29 12:01:50 +0530

    removed fullscreen snackbar

--- script.js
+++ script.js
@@ -4,22 +4,13 @@ const wait = async (milliseconds) => {
 };
 
 const snackbar = document.getElementById("snackbar");
-const snackbar2 = document.getElementById("fullscreen");
 const showSnackbar = async () => {
   await wait(7000)
   snackbar.className = "show";
 };
 
-
-const askForFullScreen = async () => {
-  snackbar.className = snackbar.className.replace("show", "");
-  snackbar2.className = "show";
-  await wait(7000);
-  snackbar2.className = snackbar2.className.replace("show", "");
-};
-
 const showFullScreen = () => {
-  snackbar2.className = snackbar2.className.replace("show", "");
+  snackbar.className = snackbar.className.replace("show", "");
   let element = document.documentElement;
   if (element.requestFullscreen) {
     element.requestFullscreen();
